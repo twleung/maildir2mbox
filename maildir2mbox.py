eccd4a52df7d2efc76ebc669178200a4be7466c0
Ted Leung <twl@sauria.com>
2024-06-13 16:52:26 -0700

    “Fixed” unicode handling
    Call encode instead of ascii, and swallow exceptions buried deep in python’s mail message code.  This swallowing will cause loss of messages

--- maildir2mbox.py
+++ maildir2mbox.py
@@ -21,43 +21,48 @@ The file is under no license/public domain. See LICENSE.txt for more details.
 
 import os, sys, argparse, mailbox, datetime
 from typing import Optional
-from  pathlib import Path
+from pathlib import Path
+
 
 def info(*args):
     # type: (*str) -> None
-    '''Display informative message'''
+    """Display informative message"""
     prefix = datetime.datetime.now().time().replace(microsecond=0).isoformat()
     print(prefix, *args)
 
+
 def error(*args):
     # type: (*str) -> None
-    '''Display error message'''
+    """Display error message"""
     prefix = datetime.datetime.now().time().replace(microsecond=0).isoformat()
-    print('ERROR', prefix, *args)
+    print("ERROR", prefix, *args)
 
 
 def maildir2mailbox(maildir_path, mbox_path):
     # type: (Path, Path) -> int
 
     if not maildir_path.exists():
-        error('maildir directory %s does not exist' % maildir_path)
+        error("maildir directory %s does not exist" % maildir_path)
         return 1
 
-    if not ((maildir_path/'cur').exists() and (maildir_path/'new').exists()):
-        error('Missing `new` and/or `cur` subdirectories in path %s, aborting conversion' % maildir_path)
+    if not ((maildir_path / "cur").exists() and (maildir_path / "new").exists()):
+        error(
+            "Missing `new` and/or `cur` subdirectories in path %s, aborting conversion"
+            % maildir_path
+        )
         return 1
 
-    mboxdir_path = Path('%s.sbd' % mbox_path)
+    mboxdir_path = Path("%s.sbd" % mbox_path)
     if not mboxdir_path.exists():
         mboxdir_path.mkdir(parents=True, exist_ok=True)
     elif mbox_path.is_dir():
-        error('%s exists but is not a directory!' % mbox_path)
+        error("%s exists but is not a directory!" % mbox_path)
         return 1
 
-    info('%s -> %s' % (maildir_path, mbox_path))
+    info("%s -> %s" % (maildir_path, mbox_path))
 
     maildir = None  # type: Optional[mailbox.Maildir]
-    mbox = None     # type: Optional[mailbox.mbox]
+    mbox = None  # type: Optional[mailbox.mbox]
     try:
         maildir = mailbox.Maildir(str(maildir_path))
         mails = len(maildir)
@@ -66,23 +71,23 @@ def maildir2mailbox(maildir_path, mbox_path):
             return 0
 
         if mbox_path.exists():
-            info('Using existing mbox file and adding the messages to it.')
+            info("Using existing mbox file and adding the messages to it.")
 
         mbox = mailbox.mbox(str(mbox_path))
         mbox.lock()
 
         # iterate over messages in the maildir and add to the mbox
-        info('Processing %d messages in %s' % (mails, maildir_path))
+        info("Processing %d messages in %s" % (mails, maildir_path))
         for i, v in enumerate(maildir.iteritems()):
             key, msg = v
             if (i % 100) == 99:
-                info('Progress: msg %d of %d' % (i+1, mails))
+                info("Progress: msg %d of %d" % (i + 1, mails))
             try:
                 mbox.add(msg)
             except Exception:
-                error('Exception while processing msg with key: %s' % key)
-                #traceback.print_exc()
-                raise
+                info("Exception while processing msg with key: %s" % key)
+                # traceback.print_exc()
+                # raise
     finally:
         # close and unlock
         if mbox:
@@ -92,9 +97,10 @@ def maildir2mailbox(maildir_path, mbox_path):
 
     return 0
 
+
 def convert(maildir_path, mbox_path, recurse):
     # type: (Path, Path, bool) -> int
-    """ Convert maildirs to mbox
+    """Convert maildirs to mbox
 
     maildir_path: path to the maildir directory containing new, cur and tmp directories
     mbox_path: path to the mbox file, already existing or to be created.
@@ -137,12 +143,17 @@ def convert(maildir_path, mbox_path, recurse):
         return result
 
     # look for the directories starting just like maildir_path
-    mdp_prefix = maildir_path.parts[-1] + '.'
-    maildir_sub_path = [ p for p in maildir_path.parent.iterdir()
-                            if p.name != mdp_prefix[:-1] and p.name.startswith(mdp_prefix)
-                            ]
-    maildir_sub_path2 = [p for p in maildir_sub_path 
-                        if p.is_dir() and (p/'cur').exists() and (p/'new').exists()]
+    mdp_prefix = maildir_path.parts[-1] + "."
+    maildir_sub_path = [
+        p
+        for p in maildir_path.parent.iterdir()
+        if p.name != mdp_prefix[:-1] and p.name.startswith(mdp_prefix)
+    ]
+    maildir_sub_path2 = [
+        p
+        for p in maildir_sub_path
+        if p.is_dir() and (p / "cur").exists() and (p / "new").exists()
+    ]
 
     # .INBOX.toto
     # .INBOX.toto.titi
@@ -154,12 +165,15 @@ def convert(maildir_path, mbox_path, recurse):
     # mbox_toto.sbd/titi.sbd/tutu.sbd
     # mbox_toto.sbd/titi.sbd/tutu
     for subdir in maildir_sub_path2:
-        mbox_dir_sub_path = Path(str(mbox_path) + subdir.name[len(mdp_prefix)-1:].replace('.', '.sbd/')+'.sbd')
-        mbox_sub_path = Path(str(mbox_dir_sub_path)[:-4])
+        mbox_dir_sub_path = Path(
+            mbox_path.encode("utf-8")
+            + subdir.name[len(mdp_prefix) - 1 :].replace(".", ".sbd/")
+            + ".sbd"
+        )
+        mbox_sub_path = Path((mbox_dir_sub_path.encode("utf-8"))[:-4])
         mbox_dir_sub_path.mkdir(parents=True, exist_ok=True)
-        info('%s -> %s' % (subdir, mbox_sub_path))
+        info("%s -> %s" % (subdir, mbox_sub_path))
         result += maildir2mailbox(subdir, mbox_sub_path)
-
 
     # .INBOX.toto/.coincoin
     # .INBOX.toto/.coincoin/.coucou
@@ -169,36 +183,57 @@ def convert(maildir_path, mbox_path, recurse):
     # mbox_toto.sbd/coincoin.sbd
     # mbox_toto.sbd/coincoin.sbd/coucou
     # mbox_toto.sbd/coincoin.sbd/coucou.sbd
-    mdp_prefix = maildir_path.parts[-1] + '.'
-    maildir_sub_path = [ (Path(dirinfo[0])/subdir).relative_to(maildir_path) for dirinfo in os.walk(str(maildir_path))
-                                        for subdir in dirinfo[1] 
-                                            if subdir.startswith('.') ]
+    mdp_prefix = maildir_path.parts[-1] + "."
+    maildir_sub_path = [
+        (Path(dirinfo[0]) / subdir).relative_to(maildir_path)
+        for dirinfo in os.walk(str(maildir_path))
+        for subdir in dirinfo[1]
+        if subdir.startswith(".")
+    ]
     for subdir in maildir_sub_path:
-        mbox_dir_sub_path = Path(str(mbox_path) + '.sbd/' + subdir.as_posix()[1:].replace('/.', '.sbd/')+'.sbd')
-        mbox_sub_path = Path(str(mbox_dir_sub_path)[:-4])
+        mbox_dir_sub_path = Path(
+            str(mbox_path)
+            + ".sbd/"
+            + subdir.as_posix()[1:].replace("/.", ".sbd/")
+            + ".sbd"
+        )
+        mbox_sub_path = Path((str(mbox_dir_sub_path))[:-4])
         mbox_dir_sub_path.mkdir(parents=True, exist_ok=True)
         result += maildir2mailbox(maildir_path / subdir, mbox_sub_path)
 
     if result > 0:
-        info('Done with %d errors.' % result)
+        info("Done with %d errors." % result)
     else:
-        info('Done')
+        info("Done")
     return result
 
-if __name__ == '__main__':
-    if sys.version_info[:2] < (3,5):
-        sys.stderr.write('This program needs at least Python 3.5 to work\r\n')
+
+if __name__ == "__main__":
+    if sys.version_info[:2] < (3, 5):
+        sys.stderr.write("This program needs at least Python 3.5 to work\r\n")
         sys.exit(1)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('maildir_path',
-                        help=("path to the existing maildir "
-                              "(containing new, cur, tmp) subdirectories"))
-    parser.add_argument('mbox_filename',
-                        help=("target filename in the mbox format. If the mailbox already exists, new messages are appended to it." ))
-    parser.add_argument('-r', '--recurse', dest='recurse', help="Process all mail folders included in maildir_path. An equivalent "
-                        "structure is recreated in the mbox format", 
-                        action='store_true')
+    parser.add_argument(
+        "maildir_path",
+        help=(
+            "path to the existing maildir " "(containing new, cur, tmp) subdirectories"
+        ),
+    )
+    parser.add_argument(
+        "mbox_filename",
+        help=(
+            "target filename in the mbox format. If the mailbox already exists, new messages are appended to it."
+        ),
+    )
+    parser.add_argument(
+        "-r",
+        "--recurse",
+        dest="recurse",
+        help="Process all mail folders included in maildir_path. An equivalent "
+        "structure is recreated in the mbox format",
+        action="store_true",
+    )
     args = parser.parse_args()
 
     sys.exit(
